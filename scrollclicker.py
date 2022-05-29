205f9f86882ef6e20e3bed66ca4b62fae56d9e72
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-05-28 21:56:24 -0400

    Update scrollclicker.py

--- scrollclicker.py
+++ scrollclicker.py
@@ -1,1 +1,14 @@
-
+# number of click per schroll
+number = 1
+# code
+import pyautogui
+from pynput.mouse import Listener, Controller, Button
+pyautogui.FAILSAFE = True
+mousee = Controller()
+button = Button.left
+def on_scroll(x, y, dx, dy):
+    for i in range(number):mousee.click(button)
+    
+mouse = Listener(on_scroll=on_scroll)
+mouse.start()
+mouse.join()
