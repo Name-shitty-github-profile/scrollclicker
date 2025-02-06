1aab2a9887d98eb80760d0197e66a38ad686eaf8
Name <102274923+Name-shitty-github-profile@users.noreply.github.com>
2025-02-06 13:05:53 -0500

    Optimization
    Basicaly changes the func if you have only one number which saves time

--- scrollclicker.py
+++ scrollclicker.py
@@ -4,10 +4,15 @@ number = 1
 import pyautogui
 from pynput.mouse import Listener, Controller, Button
 pyautogui.FAILSAFE = True
-mousee = Controller()
+ctrl = Controller()
 button = Button.left
-def on_scroll(x, y, dx, dy):
-    for i in range(number):mousee.click(button)
+if number == 1:
+    def on_scroll(x, y, dx, dy):
+        ctrl.click(button)
+else:
+    def on_scroll(x, y, dx, dy):
+        for i in range(number):
+            ctrl.click(button)
     
 mouse = Listener(on_scroll=on_scroll)
 mouse.start()
